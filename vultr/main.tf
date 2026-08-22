03a1d4732e0472a8e0196d09d9c8c831e8349cda
siddhant <siddhantkumarupmanyu@gmail.com>
2026-08-23 00:09:12 +0530

    Switch os_id to Debian 13 (trixie)

--- vultr/main.tf
+++ vultr/main.tf
@@ -3,7 +3,7 @@ resource "vultr_instance" "tarani" {
   hostname    = "tarani"
   region      = "bom"
   plan        = "vc2-1c-1gb"
-  os_id       = 2284 # Ubuntu 24.04 LTS
+  os_id       = 2625 # Debian 13 x64 (trixie)
   ssh_key_ids = var.ssh_key_ids
   tags        = ["nistaran"]
   enable_ipv6 = true
